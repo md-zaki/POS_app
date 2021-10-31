92e8ce86b02edb390468bb5dd54eae12307c4bcb
waterxiv <waterxiv@gmail.com>
2021-10-31 21:32:47 +0800

    Create ReservationApp.java

---
+++ Assignment/ReservationApp.java
@@ -0,0 +1,15 @@
+package reservationApp;
+
+public class ReservationApp {
+	private final static int MAX = 999;
+	public static void main(String[] args)
+	{
+		
+	}
+
+	public static void menuCreation()
+	{
+		//Menu[] menu = new Menu[MAX];
+		//menuItems[] menu = new menuItems[MAX];
+	}
+}
