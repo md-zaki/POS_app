1b71827f97bc84ee95bd6a6ec867fbd95423596b
waterxiv <waterxiv@gmail.com>
2021-10-31 21:29:59 +0800

    Update Table.java

--- Assignment/Table.java
+++ Assignment/Table.java
@@ -1,12 +1,10 @@
+package reservationApp;
+
 public class Table {
 
 	private int tableSize;
 	private boolean isAvailable;
 
-	/**
-	 * 
-	 * @param tableSize
-	 */
 	public Table(int tableSize) {
 		// TODO - implement Table.Table
 		throw new UnsupportedOperationException();
@@ -16,10 +14,6 @@ public class Table {
 		return this.tableSize;
 	}
 
-	/**
-	 * 
-	 * @param tableSize
-	 */
 	public void setTableSize(int tableSize) {
 		this.tableSize = tableSize;
 	}
@@ -28,10 +22,6 @@ public class Table {
 		return this.isAvailable;
 	}
 
-	/**
-	 * 
-	 * @param isAvailable
-	 */
 	public void setIsAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
