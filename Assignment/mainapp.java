2ba76c0275b26529e1e41f1fa70f137ce2fcd021
waterxiv <waterxiv@gmail.com>
2021-11-03 20:40:14 +0800

    Add files via upload

---
+++ Assignment/mainapp.java
@@ -0,0 +1,39 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class mainapp implements Serializable{
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
+    	manageStaff testStaff = new manageStaff();
+    	mainMenu testMenu = new mainMenu();
+    	Scanner scan = new Scanner(System.in);
+    	int choice;
+    	do
+    	{
+        	System.out.println("Please select your operations.");
+        	System.out.println("(1) Manage Staff");
+        	System.out.println("(2) Manage Menu");
+        	System.out.println("(6) Exit");
+        	choice = scan.nextInt();
+        	scan.nextLine();
+        	
+        	switch(choice)
+        	{
+        	case 1:
+        		testStaff.start();
+        		break;
+        	case 2:
+        		testMenu.editMenu();
+        		break;
+        	default:
+        			
+        	}
+        	
+    	} while (choice != 6);
+    	
+    	System.out.println("Program Terminating...");
+    }
+}
