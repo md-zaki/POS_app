862d0b127c4527903eddda829560c43bb50f5683
waterxiv <waterxiv@gmail.com>
2021-11-04 15:47:51 +0800

    complete implementation for staff

--- Assignment/manageStaff.java
+++ Assignment/manageStaff.java
@@ -1,13 +1,15 @@
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class manageStaff implements Serializable{
 	private ArrayList<staff> staffList = new ArrayList<staff>();
-	private static int numStaff=0;
+	//private static int numStaff=0;
 	Scanner scan = new Scanner(System.in);
 
 	public manageStaff() throws IOException, ClassNotFoundException
@@ -32,30 +34,53 @@ public class manageStaff implements Serializable{
     public void setStaffList(ArrayList<staff> staffList) {
         this.staffList = staffList;
     }
+    
+    public void saveStaffList() throws IOException
+    {
+        FileOutputStream fileOutputStream = new FileOutputStream("testStaffSave.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(staffList);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+    }
 	
+    /* getter and setter for numStaff likely not needed, use staffList.size() instead
 	public int getNumStaff()
 	{
-		return this.numStaff;
+		//return this.numStaff;
+		return staffList.size();
 	}
 	
 	public void setNumStaff(int numStaff)
 	{
-		this.numStaff = numStaff;
+		//this.numStaff = staffList.size();;
+	}
+	*/
+	
+	public int getStaffListIndexById(int staffId)
+	{
+		for (int i = 0 ; i <staffList.size(); i++)
+		{
+			if (staffList.get(i).getEmployeeId() == staffId)
+			{
+				return i;
+			}
+		}
+		return -1;
 	}
 	
-	public void addStaff()
+	public void addStaff() throws IOException
 	{
+		int conflictCheck;
 		System.out.printf("Enter Staff ID: ");
 		int staffId = scan.nextInt();
 		scan.nextLine();
 		
-		for(int i=0 ; i < staffList.size() ; i++)
+		conflictCheck = getStaffListIndexById(staffId);
+		if (conflictCheck != -1)
 		{
-			if(staffList.get(i).getEmployeeId() == staffId)
-			{
-				System.out.println("Staff with entered ID already exist.");
-				return;
-			}
+			System.out.println("Staff with entered ID already exist.");
+			return;
 		}
 		
 		System.out.printf("Enter Staff Name: ");
@@ -71,9 +96,10 @@ public class manageStaff implements Serializable{
 		staff newStaff = new staff(staffId, staffName, staffGender, staffPos);
 		
 		staffList.add(newStaff);
-		manageStaff.numStaff++;
+		//manageStaff.numStaff++;
 		
 		System.out.println("New Staff Added.");
+		saveStaffList();
 
 	}
 	
@@ -107,6 +133,7 @@ public class manageStaff implements Serializable{
 	
 	public void viewStaffById(int staffId)
 	{
+		int idExistCheck;
 		System.out.println("===============STAFF DATA===============");
 		
 		if (staffList.size() == 0)
@@ -115,48 +142,43 @@ public class manageStaff implements Serializable{
 		}
 		else
 		{
-			for (int i=0 ; i<staffList.size() ; i++)
+			idExistCheck = getStaffListIndexById(staffId);
+			if (idExistCheck == -1)
 			{
-				if (staffList.get(i).getEmployeeId() == staffId)
-				{
-					printStaff(staffList.get(i));
-					System.out.println("\n===============END OF DATA===============");
-					return;
-				}
+				System.out.println("Staff ID does not exist.");
+				System.out.println("\n===============END OF DATA===============");
 			}
-			System.out.println("Staff ID does not exist.");
-			System.out.println("\n===============END OF DATA===============");
+			else
+			{
+				printStaff(staffList.get(idExistCheck));
+				System.out.println("\n===============END OF DATA===============");
+			}		
 		}
 	}
 	
-	public void updateStaffInfo(int staffId)
+	public void updateStaffInfo(int staffId) throws IOException
 	{
-		int index = -1;
 		int choice;
-		for (int i=0; i<staffList.size(); i++)
-		{
-			if(staffList.get(i).getEmployeeId() == staffId)
-			{
-				index = i;
-			}
-		}
+		int staffIndex = getStaffListIndexById(staffId);
+		int updatedID = staffList.get(staffIndex).getEmployeeId();
 		
-		if (index == -1)
+		
+		if (staffIndex == -1)
 		{
 			System.out.println("Staff ID does not exist.");
 			return;
 		}
-		
 		do
 		{
-			staff updateStaff = new staff(staffList.get(index).getEmployeeId(), staffList.get(index).getName(), staffList.get(index).getGender(), staffList.get(index).getJobTitle());
-			System.out.println("Editing Staff " + staffList.get(index).getName() + " Information");
+			staff updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), staffList.get(staffIndex).getName(), staffList.get(staffIndex).getGender(), staffList.get(staffIndex).getJobTitle());
+			System.out.println("Editing Staff " + staffList.get(staffIndex).getName() + " Information");
 			System.out.println("Please select your operations");
 			System.out.println("(1) Edit Staff ID");
 			System.out.println("(2) Edit Staff Name");
 			System.out.println("(3) Edit Staff Gender");
 			System.out.println("(4) Edit Staff Position");
-			System.out.println("(5) Exit");
+			System.out.println("(5) View current staff information");
+			System.out.println("(6) Exit");
 			System.out.printf("Enter a choice: ");
 			choice = scan.nextInt();
 			scan.nextLine();
@@ -164,7 +186,7 @@ public class manageStaff implements Serializable{
 			{
 			case 1:
 				System.out.printf("Enter updated ID for staff: ");
-				int updatedID = scan.nextInt();
+				updatedID = scan.nextInt();
 				scan.nextLine();
 				for(int i = 0 ; i < staffList.size() ; i++)
 				{
@@ -174,25 +196,44 @@ public class manageStaff implements Serializable{
 						break;
 					}
 				}
-				updateStaff = new staff(updatedID, staffList.get(index).getName(), staffList.get(index).getGender(), staffList.get(index).getJobTitle());
-				staffList.set(index, updateStaff);
-				System.out.println("Staff " + staffList.get(index).getName() + " successfully updated with new StaffID of " + staffList.get(index).getEmployeeId());
+				updateStaff = new staff(updatedID, staffList.get(staffIndex).getName(), staffList.get(staffIndex).getGender(), staffList.get(staffIndex).getJobTitle());
+				staffList.set(staffIndex, updateStaff);
+				System.out.println("Staff " + staffList.get(staffIndex).getName() + " successfully updated with new StaffID of " + staffList.get(staffIndex).getEmployeeId());
+				saveStaffList();
+				//staffIndex = getStaffListIndexById(updatedID);
 				break;
 				
 			case 2:
 				System.out.printf("Enter updated name for staff: ");
 				String updatedName = scan.nextLine();
-				updateStaff = new staff(staffList.get(index).getEmployeeId(), updatedName, staffList.get(index).getGender(), staffList.get(index).getJobTitle());
-				staffList.set(index, updateStaff);
+				updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), updatedName, staffList.get(staffIndex).getGender(), staffList.get(staffIndex).getJobTitle());
+				staffList.set(staffIndex, updateStaff);
 				System.out.println("Staff name successfully updated to " + updatedName);
+				saveStaffList();
 				break;
 				
 			case 3:
+				System.out.printf("Enter updated gender for staff: ");
+				char updatedGender = scan.next().charAt(0);
+				scan.nextLine();
+				updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), staffList.get(staffIndex).getName(), updatedGender, staffList.get(staffIndex).getJobTitle());
+				staffList.set(staffIndex, updateStaff);
+				System.out.println("Staff gender successfully updated to " + updatedGender);
+				saveStaffList();
 				break;
 				
 			case 4:
-				break;		
+				System.out.printf("Enter updated position for staff: ");
+				String updatedPosition = scan.nextLine();
+				updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), staffList.get(staffIndex).getName(), staffList.get(staffIndex).getGender(), updatedPosition);
+				staffList.set(staffIndex, updateStaff);
+				System.out.println("Staff position successfully updated to " + updatedPosition);
+				saveStaffList();
+				break;
 			case 5:
+				viewStaffById(updatedID);
+				break;
+			case 6:
 				break;
 			default:
 				System.out.println("Please enter a valid choice");
@@ -200,49 +241,49 @@ public class manageStaff implements Serializable{
 					
 			}
 		
-		} while (choice != 5);
-		
-		
+		} while (choice != 6);
 		
 	}
 	
 	public void start() throws IOException, ClassNotFoundException
 	{
-        int choice;
-        do
-        {
-	        System.out.println("(1) View All Staffs");
-	        System.out.println("(2) View Staff by ID");
-	        System.out.println("(3) Add Staff");
-	        System.out.println("(4) Update Staff Info");
-	        System.out.println("(6) Exit");
-	        System.out.printf("Select a choice: ");
-	        choice = scan.nextInt();
-	        scan.nextLine();
-	    	switch (choice) 
-	    	{
-	            case 1: 
-	            		viewStaffList();
-	                    break;
-	            case 2:
-	            		System.out.printf("Enter Staff ID: ");
-	            		int staffId = scan.nextInt();
-	            		scan.nextLine();
-	            		viewStaffById(staffId);
-	            		break;
-	            case 3:
-	            		addStaff();
-	                    break;
-	            case 4:
-		            	System.out.printf("Enter Staff ID: ");
-	            		staffId = scan.nextInt();
-	            		scan.nextLine();
-	            		updateStaffInfo(staffId);
-	            		break;
-	            default:
-	            	//System.out.println("Please enter a valid option");
-	    	}
-        } while (choice != 6);
+		int choice;
+		do
+		{
+			System.out.println("(1) View All Staffs");
+			System.out.println("(2) View Staff by ID");
+			System.out.println("(3) Add Staff");
+			System.out.println("(4) Update Staff Info");
+			System.out.println("(6) Exit");
+			System.out.printf("Select a choice: ");
+			choice = scan.nextInt();
+			scan.nextLine();
+			switch (choice) 
+			{
+			    case 1: 
+					viewStaffList();
+				    break;
+			    case 2:
+					System.out.printf("Enter Staff ID: ");
+					int staffId = scan.nextInt();
+					scan.nextLine();
+					viewStaffById(staffId);
+					break;
+			    case 3:
+					addStaff();
+				    break;
+			    case 4:
+					System.out.printf("Enter Staff ID: ");
+					staffId = scan.nextInt();
+					scan.nextLine();
+					updateStaffInfo(staffId);
+					break;
+			    case 6:
+				break;
+			    default:
+				System.out.println("Please enter a valid option");
+			}
+		} while (choice != 6);
 		
 	}
 
