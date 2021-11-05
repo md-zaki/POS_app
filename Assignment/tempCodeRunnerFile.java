d61cc1863c1364e253f46c1b7bc258cd7843a4be
waterxiv <waterxiv@gmail.com>
2021-11-05 15:40:13 +0800

    remove redundant files

--- Assignment/tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-FileInputStream fileInputStream = new FileInputStream("testMenuSave.txt");
-        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-        mainMenu menu = (mainMenu) objectInputStream.readObject();
-        objectInputStream.close();
-        menu.editMenu();
