f70054ca0da4c0b8b5c602c74279502baa40155f
waterxiv <waterxiv@gmail.com>
2021-11-11 20:06:14 +0800

    java doc for staff, minor tweak to managestaff, making name, gender and jobtitle to all uppercase

--- OODP_Seperate/src/Entity/staff.java
+++ OODP_Seperate/src/Entity/staff.java
@@ -2,12 +2,23 @@ package Entity;
 
 import java.io.Serializable;
 
+/**
+ * Staff object for the staff members of the restaurant
+ * Every staff object has an id, gender, name and job title
+ */
 public class staff implements Serializable {
     private int employeeId;
     private char gender;
     private String name;
     private String jobTitle;
 
+    /**
+     * Constructor for Staff Object
+     * @param employeeId unique id for the staff
+     * @param name staff name
+     * @param gender staff gender
+     * @param job staff position
+     */
     public staff(int employeeId, String name, char  gender, String job)
     {
         this.employeeId = employeeId;
@@ -16,34 +27,66 @@ public class staff implements Serializable {
         this.jobTitle = job;
     }
 
+    /**
+     * Gets the employeeId of the current employee
+     * @return current employee id
+     */
     public int getEmployeeId() {
         return this.employeeId;
     }
 
+    /**
+     * Sets the id for the current employee
+     * @param employeeId new employee id for the current employee
+     */
     public void setEmployeeId(int employeeId) {
         this.employeeId = employeeId;
     }
 
+    /**
+     * Gets the gender of the current employee
+     * @return current employee's gender
+     */
     public char getGender() {
         return this.gender;
     }
 
+    /**
+     * Sets the gender for the current employee
+     * @param gender new gender for the current employee
+     */
     public void setGender(char gender) {
         this.gender = gender;
     }
 
+    /**
+     * Gets the name of the current employee
+     * @return name of the current employee
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the name for the current employee
+     * @param name new name for the current employee
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the job title for the current employee
+     * @return job title for the current employee
+     */
     public String getJobTitle() {
         return this.jobTitle;
     }
 
+    /**
+     * Sets the job title for the current employee
+     * @param jobTitle new job title for the current employee
+     */
     public void setJobTitle(String jobTitle) {
         this.jobTitle = jobTitle;
     }
--- OODP_Seperate/src/Manager/manageStaff.java
+++ OODP_Seperate/src/Manager/manageStaff.java
@@ -13,7 +13,7 @@ import Entity.staff;
 
 /**
  * Manager Class for Staff
- * @author Ng Ju Khang
+ * @author Ju Khang, Zaki
  * @version 1.0
  * @since 2021-11-11
  */
@@ -39,10 +39,15 @@ public class manageStaff implements Serializable{
         return this.staffList;
     }
 
-    public void setStaffList(ArrayList<staff> staffList) {
-        this.staffList = staffList;
-    }
+    // Looks like not needed
+    //public void setStaffList(ArrayList<staff> staffList) {
+    //    this.staffList = staffList;
+    //}
 
+    /**
+     * Function to save current staff list to a txt file
+     * @throws IOException
+     */
     public void saveStaffList() throws IOException
     {
         FileOutputStream fileOutputStream = new FileOutputStream("testStaffSave.txt");
@@ -52,6 +57,12 @@ public class manageStaff implements Serializable{
         objectOutputStream.close();
     }
 
+    /**
+     * Function to populate staff list by reading from a txt file
+     * @return ArrayList of staff members
+     * @throws ClassNotFoundException
+     * @throws IOException when not able to find the txt file to read
+     */
     public manageStaff readStaff() throws ClassNotFoundException, IOException
     {
         FileInputStream fileInputStream = new FileInputStream("testStaffSave.txt");
@@ -61,6 +72,11 @@ public class manageStaff implements Serializable{
         return staffList;
     }
 
+    /**
+     * Gets the index for which the staffId we are looking for in the array list
+     * @param staffId employeeId
+     * @return integer array index that belongs to the staffId
+     */
     public int getStaffListIndexById(int staffId)
     {
         for (int i = 0 ; i <staffList.size(); i++)
@@ -73,6 +89,10 @@ public class manageStaff implements Serializable{
         return -1;
     }
 
+    /**
+     * Function to add staff member into the staffList array list
+     * @throws IOException when not able to find the staff txt file
+     */
     public void addStaff() throws IOException
     {
         Scanner scan = new Scanner(System.in);
@@ -90,24 +110,30 @@ public class manageStaff implements Serializable{
 
         System.out.printf("Enter Staff Name: ");
         String staffName = scan.nextLine();
+        staffName.toUpperCase();
 
         System.out.printf("Enter Staff Gender (M/F): ");
         char staffGender = scan.next().charAt(0);
+        Character.toUpperCase(staffGender);
         scan.nextLine();
 
         System.out.printf("Enter Staff Job Title: ");
         String staffPos = scan.nextLine();
+        staffPos.toUpperCase();
 
         staff newStaff = new staff(staffId, staffName, staffGender, staffPos);
 
         staffList.add(newStaff);
-        //manageStaff.numStaff++;
 
         System.out.println("New Staff Added.");
         saveStaffList();
 
     }
 
+    /**
+     * Prints the details of the staff object
+     * @param staffMem staff object
+     */
     public void printStaff(staff staffMem) {
         System.out.println();
         System.out.println("Staff ID: " + staffMem.getEmployeeId());
@@ -116,6 +142,9 @@ public class manageStaff implements Serializable{
         System.out.println("Staff Position: " + staffMem.getJobTitle());
     }
 
+    /**
+     * Loops through the staffList array list and calls the printStaff function for each staff object
+     */
     public void viewStaffList()
     {
         System.out.println("===============LIST OF STAFF===============");
@@ -136,6 +165,10 @@ public class manageStaff implements Serializable{
         System.out.println("\n===============END OF LIST=================");
     }
 
+    /**
+     * Prints the details of a single staff object given by the staffId
+     * @param staffId employeeId
+     */
     public void viewStaffById(int staffId)
     {
         int idExistCheck;
@@ -162,6 +195,13 @@ public class manageStaff implements Serializable{
         }
     }
 
+    /**
+     * Updates the information of a staff object given the staffId
+     * Able to choose which attributes out of all existing attributes to edit
+     * After editing any information, the save function will be triggered to save the staffList array into the txt file
+     * @param staffId employeeId
+     * @throws IOException
+     */
     public void updateStaffInfo(int staffId) throws IOException
     {
         Scanner scan = new Scanner(System.in);
@@ -207,12 +247,11 @@ public class manageStaff implements Serializable{
                     staffList.set(staffIndex, updateStaff);
                     System.out.println("Staff " + staffList.get(staffIndex).getName() + " successfully updated with new StaffID of " + staffList.get(staffIndex).getEmployeeId());
                     saveStaffList();
-                    //staffIndex = getStaffListIndexById(updatedID);
                     break;
 
                 case 2:
                     System.out.printf("Enter updated name for staff: ");
-                    String updatedName = scan.nextLine();
+                    String updatedName = scan.nextLine().toUpperCase();
                     updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), updatedName, staffList.get(staffIndex).getGender(), staffList.get(staffIndex).getJobTitle());
                     staffList.set(staffIndex, updateStaff);
                     System.out.println("Staff name successfully updated to " + updatedName);
@@ -222,6 +261,7 @@ public class manageStaff implements Serializable{
                 case 3:
                     System.out.printf("Enter updated gender for staff: ");
                     char updatedGender = scan.next().charAt(0);
+                    Character.toUpperCase(updatedGender);
                     scan.nextLine();
                     updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), staffList.get(staffIndex).getName(), updatedGender, staffList.get(staffIndex).getJobTitle());
                     staffList.set(staffIndex, updateStaff);
@@ -231,7 +271,7 @@ public class manageStaff implements Serializable{
 
                 case 4:
                     System.out.printf("Enter updated position for staff: ");
-                    String updatedPosition = scan.nextLine();
+                    String updatedPosition = scan.nextLine().toUpperCase();
                     updateStaff = new staff(staffList.get(staffIndex).getEmployeeId(), staffList.get(staffIndex).getName(), staffList.get(staffIndex).getGender(), updatedPosition);
                     staffList.set(staffIndex, updateStaff);
                     System.out.println("Staff position successfully updated to " + updatedPosition);
@@ -251,7 +291,4 @@ public class manageStaff implements Serializable{
         } while (choice != 6);
 
     }
-
-    
-
 }
